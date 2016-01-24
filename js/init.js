c42ddec1af504522096096d9a5552ea509e21d76
FredyMV <FredyMV>
2016-01-23 22:41:25 -0500

    Quitando bootrstarp y colocando Materialize xD

---
+++ js/init.js
@@ -0,0 +1,11 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+	$('.slider').slider({
+		full_width: true
+	});
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
